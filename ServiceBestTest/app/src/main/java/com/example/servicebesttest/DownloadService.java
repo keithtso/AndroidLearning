97758b74201abfdab04fd05608af6c97193f2f72
Keith <v_cyhcao@tencent.com>
2024-06-12 01:33:09 +0800

    add Service practice code: DownloadListener, DownloadService and DownloadTask

---
+++ ServiceBestTest/app/src/main/java/com/example/servicebesttest/DownloadService.java
@@ -0,0 +1,128 @@
+package com.example.servicebesttest;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import java.io.File;
+
+import androidx.core.app.NotificationCompat;
+
+public class DownloadService extends Service {
+
+    private DownloadTask downloadTask;
+
+    private String downloadURL;
+
+    private DownloadListener listener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+            getNotificationManager().notify(1,getNotification("Downloadding...",progress));
+        }
+
+        @Override
+        public void onSuccess() {
+            downloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Success", -1));
+            Toast.makeText(DownloadService.this,"Download Success",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed() {
+            downloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Failed", -1));
+            Toast.makeText(DownloadService.this,"Download Failed",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPaused() {
+            downloadTask = null;
+            Toast.makeText(DownloadService.this,"Download Paused",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCanceled() {
+            downloadTask = null;
+            stopForeground(true);
+            Toast.makeText(DownloadService.this,"Download Canceled",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    public DownloadService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    class DownloadBinder extends Binder {
+        @SuppressLint("ForegroundServiceType")
+        public void startDownload(String url) {
+            if (downloadTask == null) {
+                downloadURL = url;
+                downloadTask = new DownloadTask(listener);
+                downloadTask.execute(downloadURL);
+                startForeground(1,getNotification("Downloading...",0));
+                Toast.makeText(DownloadService.this, "Downloading...",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void pauseDownload() {
+            if (downloadTask != null) {
+                downloadTask.pauseDownload();
+            }
+        }
+
+        public void cancelDownload() {
+            if (downloadTask != null) {
+                downloadTask.cancelDownload();
+            } else {
+                if (downloadURL != null) {
+                    String fileName = downloadURL.substring(downloadURL.lastIndexOf("/"));
+                    String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                    File file = new File(dir+fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    getNotificationManager().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownloadService.this,"Canceled",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setContentIntent(pendingIntent);
+        builder.setContentTitle(title);
+        if (progress > 0) {
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress,false);
+        }
+
+        return builder.build();
+    }
+}
---
+++ ServiceBestTest/app/src/main/java/com/example/servicebesttest/DownloadTask.java
@@ -0,0 +1,178 @@
+package com.example.servicebesttest;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
+   public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+
+    private boolean isCanceled = false;
+
+    private boolean isPaused = false;
+
+    private int lastProgress;
+
+    public DownloadTask(DownloadListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Log.d("ddd", "onPreExecute: pre");
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        InputStream is = null;
+        RandomAccessFile savedFile = null;
+        File file = null;
+
+        try {
+            long downloadLength = 0;
+            String downloadURL = strings[0];
+            String fileName = downloadURL.substring(downloadURL.lastIndexOf("/"));
+            String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+            file = new File(dir+fileName);
+            if (file.exists()) {
+                downloadLength = file.length();
+            }
+            long contentLength = getContentLength(downloadURL);
+            if (contentLength == 0) {
+                return TYPE_FAILED;
+            } else if (contentLength == downloadLength) {
+                return TYPE_SUCCESS;
+            }
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .addHeader("RANGE","bytes="+downloadLength+"-") //断点续传
+                    .url(downloadURL).build();
+            Response response = client.newCall(request).execute();
+            if (response != null) {
+                is = response.body().byteStream();
+                savedFile = new RandomAccessFile(file,"rw");
+                savedFile.seek(downloadLength);// 跳过已下载的字节
+                byte[] bytes = new byte[1024];
+                int total = 0;
+                int len;
+                while ((len = is.read(bytes)) != -1) {
+                    if (isCanceled) {
+                        return TYPE_CANCELED;
+                    } else if (isPaused) {
+                        return TYPE_PAUSED;
+                    } else {
+                        total += len;
+                        savedFile.write(bytes,0,len);
+                        int progress = (int) ((total + downloadLength) * 100 / contentLength);
+                        publishProgress(progress);
+                    }
+                }
+
+                response.body().close();
+                return TYPE_SUCCESS;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+
+                if (savedFile != null) {
+                    savedFile.close();
+                }
+
+                if (isCanceled && file != null) {
+                    file.delete();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        Log.d("ddd", "doInBackground: do in background");
+        return TYPE_FAILED;
+    }
+
+    private long getContentLength(String downloadURL) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(downloadURL).build();
+        Response response = client.newCall(request).execute();
+        if (response != null && response.isSuccessful()) {
+            long contentLength = response.body().contentLength();
+            response.close();
+            return contentLength;
+        }
+
+        return 0;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        int progress = values[0];
+        if (progress > lastProgress) {
+            listener.onProgress(progress);
+            lastProgress = progress;
+        }
+        Log.d("ddd", "onProgressUpdate: update progress:" + lastProgress);
+
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+        switch (integer) {
+            case TYPE_SUCCESS:
+                listener.onSuccess();
+                break;
+            case TYPE_FAILED:
+                listener.onFailed();
+                break;
+            case TYPE_PAUSED:
+                listener.onPaused();
+                break;
+            case TYPE_CANCELED:
+                listener.onCanceled();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void pauseDownload() {
+        isPaused = true;
+    }
+
+    public void cancelDownload() {
+        isCanceled = true;
+    }
+}
+
+//class ConcurrentTask implements Executor {
+//
+//    @Override
+//    public void execute(Runnable command) {
+//
+//    }
+//}
