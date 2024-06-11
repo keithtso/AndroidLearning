16f6edfd6148646b5bc5577e12ee73f580692e56
Keith <v_cyhcao@tencent.com>
2024-06-11 23:40:32 +0800

    add network code

---
+++ httpRequestTest/app/src/main/java/com/example/httprequesttest/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.httprequesttest;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView responseView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        WebView webView = findViewById(R.id.web_view);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.setWebViewClient(new WebViewClient());
+//        webView.loadUrl("http://www.baidu.com");
+
+        Button button = findViewById(R.id.send_request);
+        button.setOnClickListener(this);
+        responseView = findViewById(R.id.response_view);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        sendRequestWithHttpURLConnection();
+        sendRequestWithOKHttp();
+    }
+
+
+    private void sendRequestWithHttpURLConnection() {
+        // Start a new thread to make request
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection httpURLConnection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("http://www.baidu.com");
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setConnectTimeout(8000); // timeout in milliseconds
+                    httpURLConnection.setReadTimeout(8000);
+                    InputStream inputStream = httpURLConnection.getInputStream();
+
+                    // read bufferdata from the inputStream
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    showResponseText(response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (httpURLConnection != null) {
+                        httpURLConnection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void sendRequestWithOKHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://www.baidu.com").build();
+                    Response response = client.newCall(request).execute();
+                    String responseDataStr = response.body().string();
+                    showResponseText(responseDataStr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void showResponseText(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseView.setText(response);
+            }
+        });
+    }
+}
