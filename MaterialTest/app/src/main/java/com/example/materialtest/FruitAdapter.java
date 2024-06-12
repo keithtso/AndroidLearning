61e159d0637bb8d0c63977744e681bc3726959c9
Keith <v_cyhcao@tencent.com>
2024-06-13 01:43:06 +0800

    add cardView code to MaterialTest

---
+++ MaterialTest/app/src/main/java/com/example/materialtest/FruitAdapter.java
@@ -0,0 +1,71 @@
+package com.example.materialtest;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImageView;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view;
+            fruitImageView = view.findViewById(R.id.fruit_imageView);
+            fruitName = view.findViewById(R.id.fruit_name_text);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+
+    @NonNull
+    @Override
+    public FruitAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (mContext == null) {
+            mContext = parent.getContext();
+        }
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FruitAdapter.ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitName.setText(fruit.getName());
+//        Glide.with(mContext).load(fruit.getImageID()).into(holder.fruitImageView);
+        Bitmap bitmap = BitmapFactory.decodeResource(holder.fruitImageView.getResources(),fruit.getImageID());
+        holder.fruitImageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mFruitList.size();
+    }
+}
--- MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.materialtest;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
+    private Fruit[] fruits ={new Fruit("Apple", R.drawable.apple),
+            new Fruit("banana", R.drawable.banana),
+            new Fruit("orange", R.drawable.orange),
+            new Fruit("watermelon", R.drawable.watermelon),
+            new Fruit("pear", R.drawable.pear),
+            new Fruit("grape", R.drawable.grape),
+            new Fruit("pineapple", R.drawable.pineapple),
+            new Fruit("strawberry", R.drawable.strawberry),
+            new Fruit("cherry", R.drawable.cherry),
+            new Fruit("mango", R.drawable.mango)
+    };;
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +62,83 @@ public class MainActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
+        setupActionBar();
+
+        setupNavigationView();
+
+        setupFloatActionButton();
+
+        createFruitList();
+
+        setupRecycleView();
+    }
+
+    private void setupRecycleView() {
+        RecyclerView recyclerView = findViewById(R.id.recycle_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupFloatActionButton() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(v -> {
+            Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT)
+                    .setAction("Undo", v2 ->{
+                        Toast.makeText(MainActivity.this,"Data restored",Toast.LENGTH_SHORT).show();
+                    }).show();
+        });
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // 设置显示HomeAsUp按键
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            // 设置HomeAsUp按键的图片
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_settings);
+        }
+    }
+
+    private void setupNavigationView() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        com.google.android.material.navigation.NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            mDrawerLayout.closeDrawers();
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.backup) {
+            Toast.makeText(this,"Clicked on Backup",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.delete) {
+            Toast.makeText(this,"Clicked on Delete",Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.setting) {
+            Toast.makeText(this,"Clicked on Setting",Toast.LENGTH_SHORT).show();
+        } else if (id == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START,true);
+        }
+        return true;
+    }
+
+    private void createFruitList() {
+        fruitList.clear();
+
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
 }
