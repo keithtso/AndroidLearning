191c989e47466debe55a3a608b0c9f6ec4c275c3
Keith <keithcao353@gmail.com>
2024-06-16 14:36:42 +0800

    add litepal db

---
+++ PracticeCode/WeatherPractice/app/src/main/java/com/example/weatherpractice/db/County.java
@@ -0,0 +1,42 @@
+package com.example.weatherpractice.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String CountyName;
+    private String weatherID;
+    private int cityID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public String getWeatherID() {
+        return weatherID;
+    }
+
+    public void setWeatherID(String weatherID) {
+        this.weatherID = weatherID;
+    }
+
+    public int getCityID() {
+        return cityID;
+    }
+
+    public void setCityID(int cityID) {
+        this.cityID = cityID;
+    }
+}
